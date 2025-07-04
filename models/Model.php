434aba6cd3f835a3ab4785222e4d26612671a0b5
Akil Ismail <12010232@students.liu.edu.lb>
2025-07-04 16:03:35 +0300

    delete model

--- models/Model.php
+++ models/Model.php
@@ -1,14 +1,18 @@
-<?php 
-abstract class Model{
+<?php
+abstract class Model
+{
 
     protected static string $table;
     protected static string $primary_key = "id";
 
-    public static function find(mysqli $mysqli, int $id){
-        $sql = sprintf("Select * from %s WHERE %s = ?", 
-                        static::$table, 
-                        static::$primary_key);
-        
+    public static function find(mysqli $mysqli, int $id)
+    {
+        $sql = sprintf(
+            "Select * from %s WHERE %s = ?",
+            static::$table,
+            static::$primary_key
+        );
+
         $query = $mysqli->prepare($sql);
         $query->bind_param("i", $id);
         $query->execute();
@@ -18,20 +22,36 @@ abstract class Model{
         return $data ? new static($data) : null;
     }
 
-    public static function all(mysqli $mysqli){
+    public static function all(mysqli $mysqli)
+    {
         $sql = sprintf("Select * from %s", static::$table);
-        
+
         $query = $mysqli->prepare($sql);
         $query->execute();
 
         $data = $query->get_result();
 
         $objects = [];
-        while($row = $data->fetch_assoc()){
+        while ($row = $data->fetch_assoc()) {
             $objects[] = new static($row); //creating an object of type "static" / "parent" and adding the object to the array
         }
 
         return $objects; //we are returning an array of objects!!!!!!!!
+    }
+
+    public static function delete(mysqli $mysqli, int $id)
+    {
+        $sql = sprintf(
+            "Delete * from %s WHERE %s=?",
+            static::$table,
+            static::$primary_key
+        );
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("i", $id);
+        $query->execute();
+
+        $data = $query->get_result()->fetch_assoc();
+        return $data ? new static($data) : null;
     }
 
     //you have to continue with the same mindset
@@ -41,6 +61,3 @@ abstract class Model{
     //2- create() -> static function
     //3- delete() -> static function 
 }
-
-
-
